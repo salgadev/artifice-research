b34005f7807a1f7258476f9e66e5232ae2a0d921
Carlos Salgado <csalgado@uwo.ca>
2024-09-19 13:57:37 -0400

    Change copy to reflect dsp focus, add logo and vision icon

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -2,7 +2,6 @@ import { component$ } from "@builder.io/qwik";
 
 // @ts-ignore
 import logoSrc from "~/assets/images/a_logo.png?width=64&height=64&png";
-//import logoSrc from "~/assets/images/alogo.jpeg?width=64&height=64&jpeg";
 
 export default component$(() => (
   <span class="self-center ml-2 text-2xl md:text-xl font-bold text-gray-900 whitespace-nowrap dark:text-white flex items-center">
